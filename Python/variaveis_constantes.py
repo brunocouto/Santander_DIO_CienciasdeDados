0028744d39832f781c43c12cf9b9ee5e853ee948
seuUserNamebrunocouto <bruno.couto.joao@gmail.com@gmail.com>
2023-08-21 19:30:14 -0300

    introdução PY

---
+++ Python/variaveis_constantes.py
@@ -0,0 +1,27 @@
+# nome ="Guilherme"
+# idade = 28
+
+# nome = "Giovania"
+# limit_saque_diario =1000
+
+# STATES = {"SP","RJ","SC","RS"}
+
+# print(int(1.9734))
+# print(int("10"))
+# print(float("10.10"))
+
+# valor = 10
+# valor_str =str(valor)
+# print(type(valor))
+# print(type(valor_str))
+
+# print(100/2)
+# print(100//2)
+
+nome = input("informe seu nome")
+idade =input("informe sua idade")
+
+print(nome , idade, end="...\n")
+print(nome , idade , sep="#")
+print(nome , idade, sep="#" ,end="...\n")
+
